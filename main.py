4757fb5f03f037ed536a870fc31e2f94c838cd44
Anna Cai <anana10c@gmail.com>
2018-08-02 10:21:29 -0400

    train gameboy

--- main.py
+++ main.py
@@ -37,18 +37,18 @@ def fetch_train_thoughts(m,pcs,batches,name="trainthoughts"):
 		all_thoughts.append((ipt,opt,thoughts))
 	pickle.dump(all_thoughts, open('output/'+name+'.p','wb'))
 
-#if __name__ == '__main__':
-def start_training():
+if __name__ == '__main__':
+#def start_training():
 	pcs = multi_training.loadPieces("gameboy-music")
 	print "pieces loaded"
-	m = model.Model([100,100],[50,25], dropout=0.5)
+	m = model.Model([100,100],[30,15], dropout=0.5)
 	print "model created"
 	multi_training.trainPiece(m, pcs, 1000)
 	print "training complete"
 	pickle.dump( m.learned_config, open( "output/final_learned_config.p", "wb" ) )
 
-if __name__ == '__main__':
-#def generate_pieces():
+#if __name__ == '__main__':
+def generate_pieces():
         pcs = multi_training.loadPieces("tetris")
         print "pieces loaded"
         m = model.Model([100,100],[30,15], dropout=0.5)
