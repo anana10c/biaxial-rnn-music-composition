4e0c335f67068d3174615ea0cb7b6c0019c5a611
Anna Cai <anana10c@gmail.com>
2018-08-02 18:54:27 -0400

    i love it when it disconnects

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
 	m = model.Model([100,100],[30,15], dropout=0.5)
 	m.learned_config = pickle.load(open( "output/params500.p", "rb" ) )
 	print "model created"
-	multi_training.trainPiece(m, pcs, 1000)
+	multi_training.trainPiece(m, pcs, 500)
 	print "training complete"
 	pickle.dump( m.learned_config, open( "output/final_learned_config.p", "wb" ) )
 
