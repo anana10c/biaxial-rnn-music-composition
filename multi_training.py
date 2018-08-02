ca56b98b242cd3283d5d5d23bf31ff34099a9a70
Anna Cai <anana10c@gmail.com>
2018-08-02 00:51:44 -0400

    ???

--- multi_training.py
+++ multi_training.py
@@ -48,12 +48,13 @@ def trainPiece(model,pieces,epochs,start=0):
         stopflag[0] = True
     old_handler = signal.signal(signal.SIGINT, signal_handler)
     for i in range(start,start+epochs):
+        print i
         if stopflag[0]:
             break
         error = model.update_fun(*getPieceBatch(pieces))
-        if i % 100 == 0:
+        if i % 10 == 0:
             print "epoch {}, error={}".format(i,error)
-        if i % 500 == 0 or (i % 100 == 0 and i < 1000):
+        if i % 100 == 0 or (i % 100 == 0 and i < 1000):
             xIpt, xOpt = map(numpy.array, getPieceSegment(pieces))
             noteStateMatrixToMidi(numpy.concatenate((numpy.expand_dims(xOpt[0], 0), model.predict_fun(batch_len, 1, xIpt[0])), axis=0),'output/sample{}'.format(i))
             pickle.dump(model.learned_config,open('output/params{}.p'.format(i), 'wb'))
