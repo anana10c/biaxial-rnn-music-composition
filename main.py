d51fcde2ee4a1f9817719c1de56d74adf1d72764
Anna Cai <anana10c@gmail.com>
2018-08-02 21:14:11 -0400

    imstupid

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
 	pcs = multi_training.loadPieces("gameboy-music")
 	print "pieces loaded"
 	m = model.Model([100,100],[30,15], dropout=0.5)
-	m.learned_config = pickle.load(open( "output/start500.p", "rb" ) )
+	m.learned_config = pickle.load(open( "output/start800.p", "rb" ) )
 	print "model created"
 	multi_training.trainPiece(m, pcs, 200)
 	print "training complete"
