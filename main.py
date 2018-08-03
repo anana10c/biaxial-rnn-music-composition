791fb53ae0e6285eb0c10a0e912d81c8c766deeb
Anna Cai <anana10c@gmail.com>
2018-08-03 11:28:11 -0400

    train gameboy2

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
 	pcs = multi_training.loadPieces("gameboy-music") #tetris and pokemon
 	print "pieces loaded"
 	m = model.Model([100,100],[30,15], dropout=0.5)
-	m.learned_config = pickle.load(open( "start-gameboyx1.p", "rb" ) )
+	m.learned_config = pickle.load(open( "start-gameboy2.p", "rb" ) )
 	print "model created"
 	multi_training.trainPiece(m, pcs, 2000)
 	print "training complete"
