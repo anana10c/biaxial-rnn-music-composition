717ab23b5a76f66c7a8bf6def266afc3bad30ca9
Anna Cai <anana10c@gmail.com>
2018-08-03 11:13:07 -0400

    forgot to save

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
 	pcs = multi_training.loadPieces("selected-gameboy") #tetris and pokemon
 	print "pieces loaded"
 	m = model.Model([100,100],[30,15], dropout=0.5)
-	#m.learned_config = pickle.load(open( "start-poketris.p", "rb" ) )
+	m.learned_config = pickle.load(open( "start-poketris.p", "rb" ) )
 	print "model created"
 	multi_training.trainPiece(m, pcs, 2000)
 	print "training complete"
